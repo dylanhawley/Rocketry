f84c480cbc98e76e719cfcbb2b39e9305b0cbad8
Dylan Hawley <dylanthomashawley@gmail.com>
2023-10-16 21:23:51 -0400

    Initial Commit

---
+++ Launch Feed/Launch_FeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Launch_FeedApp.swift
+//  Launch Feed
+//
+//  Created by Dylan Hawley on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Launch_FeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
