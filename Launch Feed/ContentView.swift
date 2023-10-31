5eedb4bbc4421880fc9fb76eb497153c76ae64bd
Dylan Hawley <dylanthomashawley@gmail.com>
2023-10-30 23:34:14 -0400

    Split Launch struct into separate file
    Launch and LL2Response moved to LL2Model Swift file

--- Launch Feed/ContentView.swift
+++ Launch Feed/ContentView.swift
@@ -7,61 +7,6 @@
 
 import SwiftUI
 
-struct Launch: Hashable, Decodable {
-    var id: String
-    var name: String
-    var last_updated: String
-    var net: String
-    var window_end: String
-    var window_start: String
-    var holdreason: String
-    var failreason: String
-    var rocket: Rocket
-    var mission: Mission
-    var pad: Pad
-    var image: String
-    
-    struct Rocket: Hashable, Decodable {
-        var configuration: RocketConfiguration
-        
-        struct RocketConfiguration: Hashable, Decodable {
-            var url: String
-            var name: String
-            var family: String
-            var full_name: String
-            var variant: String
-        }
-    }
-    
-    struct Mission: Hashable, Decodable {
-        var name: String
-        var description: String
-        var type: String
-        
-        struct Orbit: Hashable, Decodable {
-            var name: String
-            var abbrev: String
-        }
-    }
-
-    struct Pad: Hashable, Decodable {
-        var name: String
-        var map_url: String
-        var latitude: String
-        var longitude: String
-        var location: Location
-        
-        struct Location: Hashable, Decodable {
-            var name: String
-            var country_code: String
-            var timezone_name: String
-        }
-    }
-}
-
-struct LL2Response: Decodable {
-    var results: [Launch]
-}
 
 class ViewModel: ObservableObject {
     @Published var launches: [Launch] = []
