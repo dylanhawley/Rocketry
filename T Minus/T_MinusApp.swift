7aa3ab002e42d44fea31960b8fa52f20a01d4be5
Dylan Hawley <dylanthomashawley@gmail.com>
2024-07-03 21:55:46 -0400

    Add MockDataService Allows developer to quickly see UI changes without fetching from URL on each refresh.

--- T Minus/T_MinusApp.swift
+++ T Minus/T_MinusApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct T_MinusApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: ViewModel())
         }
     }
 }
