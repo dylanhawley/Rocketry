c0655f68250eb61f431d5d78ea38f0557c40dcd4
Dylan Hawley <dylanthomashawley@gmail.com>
2024-10-18 03:02:59 -0400

    Add Cloud class. And remove ghost files (maybe).

--- T Minus/Cloud.swift
+++ T Minus/Cloud.swift
@@ -2,17 +2,22 @@
 //  Cloud.swift
 //  T Minus
 //
-//  Created by Dylan Hawley on 9/23/24.
+//  Created by Dylan Hawley on 10/18/24.
 //
+
 
 import SwiftUI
 
 class Cloud {
+    enum Thickness: CaseIterable {
+        case none, thin, light, regular, thick, ultra
+    }
+
     var position: CGPoint
     let imageNumber: Int
     let speed = Double.random(in: 4...12)
     let scale: Double
-    
+
     init(imageNumber: Int, scale: Double) {
         self.imageNumber = imageNumber
         self.scale = scale
