091d0bba120fa36e8f866d36e20b0771eb17305f
Dylan Hawley <dylanthomashawley@gmail.com>
2024-10-26 00:50:48 -0400

    Use @State to update SkyView. Dynamically update gradient stops with @State.

--- T Minus/SkyView.swift
+++ T Minus/SkyView.swift
@@ -19,15 +19,6 @@ class SolarTime {
         return timeInterval / (24 * 60 * 60)
     }
     
-    /// Normalize the time of day to be in the range `[0, 1]`.
-    static func normalizeTimeOfDay(_ date: Date, _ location: CLLocationCoordinate2D) -> Double {
-        var localTimeZone: TimeZone = .current
-        SolarTime.getTimeZone(location: location) { timeZone in
-            localTimeZone = timeZone
-        }
-        return normalizeTimeOfDay(date, localTimeZone)
-    }
-    
     static func getTimeZone(location: CLLocationCoordinate2D, completion: @escaping ((TimeZone) -> Void)) {
         let cllLocation = CLLocation(latitude: location.latitude, longitude: location.longitude)
         let geocoder = CLGeocoder()
@@ -49,20 +40,38 @@ class SolarTime {
 struct SkyView: View {
     let date: Date
     let location: CLLocationCoordinate2D
-    let backgroundTopStops: [Gradient.Stop]
-    let backgroundBottomStops: [Gradient.Stop]
     
-    init(date: Date, location: CLLocationCoordinate2D) {
-        self.date = date
-        self.location = location
-        var localTimeZone: TimeZone = .current
-        SolarTime.getTimeZone(location: location) { timeZone in
-            localTimeZone = timeZone
+    @State private var backgroundTopStops: [Gradient.Stop] = []
+    @State private var backgroundBottomStops: [Gradient.Stop] = []
+    @State private var normalizedTimeOfDay: Double = 0
+    
+    var body: some View {
+        Group {
+            if !backgroundTopStops.isEmpty && !backgroundBottomStops.isEmpty {
+                LinearGradient(colors: [
+                    backgroundTopStops.interpolated(amount: normalizedTimeOfDay),
+                    backgroundBottomStops.interpolated(amount: normalizedTimeOfDay)
+                ], startPoint: .top, endPoint: .bottom)
+            } else {
+                Color(.secondarySystemBackground)
+            }
         }
-        
+        .onAppear {
+            fetchTimeZoneAndCompute()
+        }
+    }
+    
+    private func fetchTimeZoneAndCompute() {
+        SolarTime.getTimeZone(location: location) { timeZone in
+            computeGradientStops(localTimeZone: timeZone)
+        }
+    }
+    
+    private func computeGradientStops(localTimeZone: TimeZone) {
         let solar = Solar(for: date, coordinate: location)
         let daytimeStart: Date? = Calendar.current.date(byAdding: .hour, value: 1, to: (solar?.sunrise)!)
         let daytimeEnd: Date? = Calendar.current.date(byAdding: .hour, value: -1, to: (solar?.sunset)!)
+        let normalizedTime = SolarTime.normalizeTimeOfDay(date, localTimeZone)
         
         let astronomicalDawnNormalized: Double = solar?.astronomicalSunrise.map{SolarTime.normalizeTimeOfDay($0, localTimeZone)} ?? 0.25
         let astronomicalDuskNormalized: Double = solar?.astronomicalSunset.map{SolarTime.normalizeTimeOfDay($0, localTimeZone)} ?? 0.82
@@ -71,6 +80,7 @@ struct SkyView: View {
         let sunriseNormalized: Double = solar?.sunrise.map{SolarTime.normalizeTimeOfDay($0, localTimeZone)} ?? 0.33
         let sunsetNormalized: Double = solar?.sunset.map{SolarTime.normalizeTimeOfDay($0, localTimeZone)} ?? 0.78
         
+        self.normalizedTimeOfDay = normalizedTime
         self.backgroundTopStops = [
             .init(color: .midnightStart, location: 0),
             .init(color: .midnightStart, location: astronomicalDawnNormalized),
@@ -91,13 +101,6 @@ struct SkyView: View {
             .init(color: .midnightEnd, location: astronomicalDuskNormalized),
             .init(color: .midnightEnd, location: 1)
         ]
-    }
-
-    var body: some View {
-        LinearGradient(colors: [
-            backgroundTopStops.interpolated(amount: SolarTime.normalizeTimeOfDay(date, .current)),
-            backgroundBottomStops.interpolated(amount: SolarTime.normalizeTimeOfDay(date, .current))
-        ], startPoint: .top, endPoint: .bottom)
     }
 }
 
