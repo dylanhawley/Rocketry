b5946775b6bc127fdf8118f6ef92dc461aeeb04b
Dylan Hawley <dylanthomashawley@gmail.com>
2024-12-16 20:00:14 -0500

    Add placeholder for Livestream asyncimage

--- Rocketry/Views/LaunchDetailView.swift
+++ Rocketry/Views/LaunchDetailView.swift
@@ -11,11 +11,11 @@ import Solar
 struct LaunchDetailView: View {
     let launch: Launch
     @Environment(\.dismiss) private var dismiss
+    @State private var detailedLaunch: LaunchDetailed? = nil
     
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 16) {
-                // Header with background
                 VStack(alignment: .leading) {
                     Text(launch.mission)
                         .font(.largeTitle)
@@ -40,7 +40,13 @@ struct LaunchDetailView: View {
                 // Launch details
                 VStack(alignment: .leading, spacing: 24) {
                     MissionDetailsView(launch: launch)
-                    URL(string: launch.url).map { LivestreamView(source: $0) }
+                    if let detailedLaunch = detailedLaunch {
+                        let vidURLs = detailedLaunch.vidURLs
+                        if !vidURLs.isEmpty {
+                            let highestPriorityVideo = vidURLs.max(by: { $0.priority < $1.priority })
+                            LivestreamView(source: highestPriorityVideo!)
+                        }
+                    }
                     PadMapView(location: launch.location, visibility: launch.weather?.visibility)
                     launch.weather.map { ConditionsView(weather: $0) }
                 }
@@ -52,6 +58,22 @@ struct LaunchDetailView: View {
             WeatherAnimationView(launch: launch)
             .ignoresSafeArea()
         )
+        .onAppear {
+            Task { await fetchVideoURL() }
+        }
+    }
+
+    private func fetchVideoURL() async {
+        do {
+            let source = URL(string: launch.url)!
+            let (data, _) = try await URLSession.shared.data(from: source)
+            let decoded = try JSONDecoder().decode(LaunchDetailed.self, from: data)
+            await MainActor.run {
+                self.detailedLaunch = decoded
+            }
+        } catch {
+            print("Failed to fetch or decode detailed launch data: \(error)")
+        }
     }
 }
 
--- Rocketry/Views/LivestreamView.swift
+++ Rocketry/Views/LivestreamView.swift
@@ -8,58 +8,33 @@
 import SwiftUI
 
 struct LivestreamView: View {
-    let source: URL
-    
-    @State private var detailedLaunch: LaunchDetailed? = nil
+    let source: LaunchDetailed.VidURL
     
     var body: some View {
-        VStack() {
-            if let detailedLaunch = detailedLaunch {
-                let vidURLs = detailedLaunch.vidURLs
-                if !vidURLs.isEmpty {
-                    let highestPriorityVideo = vidURLs.max(by: { $0.priority < $1.priority })
-                    Link(destination: URL(string: highestPriorityVideo!.url)!) {
-                        AsyncImage(url: URL(string: highestPriorityVideo!.feature_image), transaction: Transaction(animation: .spring)) {
-                            phase in
-                            switch phase {
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    .overlay(
-                                        Image(systemName: "play.fill")
-                                            .padding()
-                                            .foregroundColor(.white)
-                                            .font(.largeTitle)
-                                            .background(.ultraThinMaterial, in: Circle())
-                                    )
-                            default:
-                                EmptyView()
-                            }
-                        }
-                        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
-                    }
+        Link(destination: URL(string: source.url)!) {
+            AsyncImage(url: URL(string: source.feature_image), transaction: Transaction(animation: .spring)) {
+                phase in
+                switch phase {
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            Image(systemName: "play.fill")
+                                .padding()
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                                .background(.ultraThinMaterial, in: Circle())
+                        )
+                default:
+                    Rectangle()
+                        .foregroundColor(.gray.opacity(0.3))
+                        .aspectRatio(16/9, contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
             }
-        }
-        .onAppear {
-            Task { await fetchVideoURL() }
-        }
-    }
-    
-    // Asynchronous function to fetch and decode detailed launch info from launch.url
-    private func fetchVideoURL() async {
-        print("fetching vidurls")
-        do {
-            let (data, _) = try await URLSession.shared.data(from: source)
-            let decoded = try JSONDecoder().decode(LaunchDetailed.self, from: data)
-            await MainActor.run {
-                self.detailedLaunch = decoded
-            }
-        } catch {
-            // Handle error (e.g., show an alert or log the error)
-            print("Failed to fetch or decode detailed launch data: \(error)")
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
         }
     }
 }
